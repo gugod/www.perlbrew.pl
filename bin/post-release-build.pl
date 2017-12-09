e59db5d64ccf63e6b0a0288feb24c9188096e26c
Kang-min Liu <gugod@gugod.org>
2017-12-09 17:42:21 +0900

    program to build release notes from Change log

---
+++ bin/post-release-build.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+use v5.18;
+use strict;
+use warnings;
+use IO::All;
+
+use constant USAGE => <<EOUSAGE;
+Usage:
+    
+    post-release-build.pl dir/to/App-perlbrew/
+
+EOUSAGE
+
+sub grok_changelog {
+    my $perlbrew_git_dir = $_[0];
+    # Take the latest section in the changelog
+    my ($version, $release_time, $updates_txt) = (undef, '');
+    my $io = io->catfile($perlbrew_git_dir, "Changes")->utf8;
+    while(my $line = $io->getline()) {
+        if ($line =~ /^([0-9]+\.[0-9]+): +# +(.+) *$/) {
+            last if ($version);
+            $version = $1;
+            $release_time = $2;
+        } else {
+            $updates_txt .= $line;
+        }
+    }
+
+    return {
+        version => $version,
+        time => $release_time,
+        updates => $updates_txt,
+    };
+}
+
+sub build_release_notes {
+    my $release_detail = $_[0];
+
+    return <<"TMPL"
+Perlbrew Release $release_detail->{version}
+=====================
+
+perlbrew $release_detail->{version} was released at $release_detail->{time} with these notable changes:
+
+$release_detail->{updates}
+
+TMPL
+}
+
+sub MAIN {
+    my $perlbrew_git_dir = $_[0] or die USAGE;
+    my $release_detail = grok_changelog $perlbrew_git_dir;
+    my $notes = build_release_notes( $release_detail );
+    my $f = io->catfile("content", "Release-" . $release_detail->{version} . ".md");
+    $f->print($notes);
+    $f->close;
+    say "BUILD: $f";
+}
+MAIN(@ARGV);
